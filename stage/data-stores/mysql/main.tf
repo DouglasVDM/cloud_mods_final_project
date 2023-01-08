40e4aec8c3030a14b5e7d59e57c95a6a005327bb
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-01-08 23:30:26 +0200

    Point backend to correct TFC org and workspace

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -23,4 +23,24 @@ terraform {
     dynamodb_table = "stage-up-and-running-locks"
     encrypt        = true
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "example-org-ba773e"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
 }
